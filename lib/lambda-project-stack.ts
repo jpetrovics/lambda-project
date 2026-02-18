1310a5b8e1a146aeea4082a0c910227c105bd9b0
Yury Petrovics <jurijs.petrovics@gmail.com>
2026-02-18 20:33:19 +0000

    - Initial project setup after running "cdk init app --language typescript"

---
+++ lib/lambda-project-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib/core';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class LambdaProjectStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'LambdaProjectQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
---
+++ test/lambda-project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaProject from '../lib/lambda-project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaProject.LambdaProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
